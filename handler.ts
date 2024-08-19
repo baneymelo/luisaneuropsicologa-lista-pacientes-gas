bc4fcb68930bd55f5137e3cbaa026d7e7db6268f
baneymelo <baneymelo@gmail.com>
2024-08-19 00:24:11 -0500

    patients with session added

--- handler.ts
+++ handler.ts
@@ -1,24 +1,41 @@
+import Sheet = GoogleAppsScript.Spreadsheet.Sheet;
+
 const run = () => {
     const spreadsSheetId = "1ZTgWI7qjW31vuiML2ODSX0FQuo-mtQ-L0-Vd7eLw2kw";
     const sheetName = "INPUT";
-    const patientList = spreadsSheetProcessor(spreadsSheetId, sheetName);
-    console.log(patientList)
+    const weekOneRanges = ["INPUT!A1:I24", "INPUT!L1:T24", "INPUT!W1:AE24", "INPUT!AH1:AP24"];
+    const weekTwoRanges = ["INPUT!A28:I51", "INPUT!L28:T51", "INPUT!W28:AE51", "INPUT!AH28:AP51"];
+    const fortnightlyRanges = [weekOneRanges, weekTwoRanges];
+    const data = spreadsSheetProcessor(spreadsSheetId, sheetName, fortnightlyRanges);
+    console.log(data)
+    //console.log(data.length)
     const documentName = "Listado de pacientes ()"
-    /* const body = documentProcessor(documentName);
-    console.log(body) */
+    //const body = documentProcessor(data, documentName);
+    //console.log(body)
 }
 
 
-const spreadsSheetProcessor = (spreadsSheetId: String, sheetName: String) => {
-    const inputSheet = Module.getInputSheet(spreadsSheetId, sheetName);
-    const inputValues = inputSheet.getDataRange().getValues();
-    const sanitizedData = Module.sanitizeByEmptyNameAndSession(inputValues);
-    const filteredData = Module.filterByCells(sanitizedData);
-    return Module.arrayToPatients(filteredData);
+const spreadsSheetProcessor = (spreadsSheetId: String,
+                               sheetName: String,
+                               fortnightlyRanges: String[][]
+    ) => {
+        const inputSheet: Sheet = Module.getInputSheet(spreadsSheetId, sheetName);
+        /*const daysWeekOne = Module.getDates(inputSheet, "INPUT!A1:AP1");
+        const daysWeekTwo = Module.getDates(inputSheet, "INPUT!A28:AP28");
+        const dates = [daysWeekOne, daysWeekTwo];
+        const datesParsed = Module.parseDateToString(dates);
+        console.log(datesParsed)*/
+        const patients = Module.getPatients(fortnightlyRanges, inputSheet);
+        //console.log(patients.data)
+        //const patientsData = Module.patientDataToString(patients.data);
+        const depuredData = Module.depureData(patients.data);
+        //const sortedData = Module.sortData(depuredData);
+        return depuredData;
 }
 
-const documentProcessor = (name: String) => {
-    return Module.createDocument(name);
+const documentProcessor = (data: string[][], name: string) => {
+    const doc = Module.createDocument(name);
+    return doc.appendTable(data);
 }
 
 
--- module.ts
+++ module.ts
@@ -1,60 +1,105 @@
 namespace Module {
+
+    export type Patient = string[];
+
     export const getInputSheet: Sheet = (id, sheetName) => {
                const ss: SpreadsheetApp = SpreadsheetApp.openById(id);
                const inputSheet: Sheet = ss.getSheetByName(sheetName);
                return inputSheet;
-   }
+    }
 
-   const filterIds = [1,2,5,8]
-   const filterByIndex = (array, index) => filterIds.includes(index);
+    export const getDates = (sheet, range) => {
+        return sheet.getRange(range)
+            .getValues()
+            .flat()
+            .filter(value => value !== '');
+    }
 
-   export const filterByCells = (inputValues: String[]) => {
-       return inputValues.reduce((acc, cur, row) => {
-           const filteredValues = cur.filter(filterByIndex);
-           acc.push(filteredValues);
-           return acc;
-       },[]);
-   }
+    export const getPatientsByDay = (sheet: Sheet, dayRange: string): string[][] => (
+        sheet.getRange(dayRange)
+            .getValues()
+            /*.filter(patient => {
+                return patient[8] === 1
+            })*/
+    );
 
-   const patientHasSession = (inputValue: String[]) => inputValue[1] !== "" && inputValue[8] !== 1
+    const formatDate = (date: Date) => {
+        const day = String(date.getDate()).padStart(2, '0');
+        const month = String(date.getMonth() + 1).padStart(2, '0'); // Enero es 0
+        const year = date.getFullYear();
+        return `${day}/${month}/${year}`;
+    }
 
-   export const sanitizeByEmptyNameAndSession = (inputValues: String[]) => (
-       inputValues.filter(inputValue => !patientHasSession(inputValue))
-   )
+    const parseDateToString = (dates: Date) => (
+        dates.map(date => formatDate(date))
+    )
 
-   const createPatient = (patientsArrayData) => ({
-           document: patientsArrayData[1],
-           name: patientsArrayData[0],
-           date: [patientsArrayData[2]],
-           session: patientsArrayData[3]
-   })
+    export const getPatients = (fortnightlyRanges: String[][], inputSheet: Sheet) => (
+       fortnightlyRanges.flat().reduce((patients, curRangeDay) => {
+           const patientsByDay = Module.getPatientsByDay(patients.sheet, curRangeDay);
+           patients.data.push(patientsByDay);
+           return patients;
+       }, {sheet: inputSheet, data: []})
+    )
 
-   export const arrayToPatients = (patientsArray) => {
-       console.log(patientsArray)
-          return patientsArray.reduce((patient, currPatient) => {
-          if(currPatient[0][0] === ''){
-            return;
-          }
-          return patient;
-       },[])
-      /* return patientsArray.reduce((patient, currPatient) => {
-          const DOCUMENT = currPatient[1];
-              if (patient[DOCUMENT]) {
-                  patient[DOCUMENT].date.push(currPatient[2]);
-                  patient[DOCUMENT].session++;
-              } else {
-                  patient[DOCUMENT] = createPatient(currPatient);
-              }
-          return patient;
-      }, {}) */
-  }
+    const depureDayBySessions = (data: Patient[]) => data.reduce((patientsDay, currPatientDay, idx) => {
+        const totalSessionsByDay = currPatientDay.map((patientRow, idx) => idx >= 2 ? patientRow[8] : 0)
+        const sessionsExist = totalSessionsByDay.some(sessionValue => sessionValue === 1);
+        if(sessionsExist){
+            patientsDay.push(currPatientDay);
+        }
+        return patientsDay;
+    },[])
 
-  export const createDocument = (name: String) => DocumentApp.create(name)
+    const depureByColumns = (data: Patient[]) => data.map(
+       patientsData => patientsData.map(
+           patient => [patient[1], patient[2], patient[8], patient[9]])
+    );
+
+    const  depureWithSessions = (data: Patient[]) => (
+       data.map(patientByDay => patientByDay.filter(patient => patient[2] === 1))
+    )
+
+    const replaceHourByDate = (data: Patient[]) => data.map(patientsDay => {
+        const date = patientsDay[0][0];
+        const dateString = formatDate(date);
+        //console.log(dateString)
+        return patientsDay.map(patient => {
+            patient.push(dateString);
+            return patient;
+        })
+    })
+
+    const groupByDocument = (data: Patient[]) => {
+        return data.reduce((patients, curPatientsDay, i) => {
+            //const patientDocument = curPatientsDay[idx][1];
+            const newPatientsDay = curPatientsDay.filter((patient, j) => {
+
+                patient[1] === patientDocument
+            });
+
+            patients.push(curPatientsDay);
+            return patients;
+        },[])
+    }
+
+
+    export const depureData = (data: Patient[]) => {
+        const patientsWithDaySessions = depureDayBySessions(data);
+        const patientsWithDates = replaceHourByDate(patientsWithDaySessions);
+        const patientsFilteredByColumns = depureByColumns(patientsWithDates);
+        const patientsWithSessions = depureWithSessions(patientsFilteredByColumns);
+        //const patientsGroupByName = groupByDocument(patientsWithSessions);
+        return patientsWithSessions;
+    };
+    //export const sortData = (data: Patient[]) => {}
+
+    export const patientDataToString = (data: Patient[]) => (
+       data
+           .flat()
+           .map(patientArr => patientArr.map(patient => patient.toString()))
+    );
+
+    export const createDocument = (name: string) => DocumentApp.create(name)
     .getBody()
-    .appendParagraph(name.toUpperCase())
-    .setAlignment(DocumentApp.HorizontalAlignment.CENTER);
-
-  export const createTable = (data: {}) => {
-
-  }
 }
